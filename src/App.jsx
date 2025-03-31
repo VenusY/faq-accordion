34b790044145b97c3f771f7c0fc41fc6a4f5442a
Venus <venusyip@outlook.com>
2025-03-31 06:44:16 +0100

    Add outline markup for FAQ card

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import QuestionsSection from './components/QuestionsSection';
+
+export default function App() {
+  return (
+    <section className='card'>
+      <h1 className='card__heading'>FAQs</h1>
+      <QuestionsSection />
+    </section>
+  );
+}

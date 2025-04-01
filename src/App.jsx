19039d4ba4331333b627326d2b06cd83e1605827
Venus <venusyip@outlook.com>
2025-04-01 05:22:37 +0100

    Add star icon to card header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './styles/reset.css';
+import './styles/scss/App.scss';
 import QuestionsSection from './components/QuestionsSection';
 
 export default function App() {
   return (
     <section className='card'>
-      <h1 className='card__heading'>FAQs</h1>
+      <header className='card__header'>
+        <img
+          src={require('./assets/images/icon-star.svg')}
+          alt='Star'
+          className='card__icon'
+        />
+        <h1 className='card__heading'>FAQs</h1>
+      </header>
+
       <QuestionsSection />
     </section>
   );
